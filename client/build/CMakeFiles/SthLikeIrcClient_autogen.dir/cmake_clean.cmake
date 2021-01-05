0e210eae114b028dca757dde702f9577a09a953e
Wiktor Górczak <wiktor.gorczak@openanalytics.eu>
2021-01-05 23:55:22 +0100

    remove build files

--- client/build/CMakeFiles/SthLikeIrcClient_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/SthLikeIrcClient_autogen"
-  "SthLikeIrcClient_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/SthLikeIrcClient_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
